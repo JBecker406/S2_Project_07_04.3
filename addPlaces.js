e4a2abf057729cfe10bae57c07a4725838663063
Unknown <JBecker406@west-mec.org>
2018-02-20 18:05:20 -0700

    Final Commit
    Added heading for index as well as a .html extension, moved script to a JavaScipt file and linked it to the index, and changed value of i to 0 and added missing bracket, semicolon, and double quote to fix bugs

---
+++ addPlaces.js
@@ -0,0 +1,30 @@
+// changed value of i to 0 and added missing bracket, semicolon, and double quote to fix bugs
+
+var places = []; // new array to store entered places
+var i = 0; // counter variable to track array indexes
+
+// function to add input to array and then generate list after 5th submission
+function processInput() {
+    places[i] = document.getElementById("placeBox").value; // add entered value to array
+    document.getElementById("placeBox").value = ""; // clear text box
+    if (i < 5) { // iterate counter variable
+        i++;
+    }
+    else { // add entered value to array and write results to document
+        document.getElementById("resultsExpl").innerHTML = "You entered the following places:";
+        listItem = "";
+        for (j = 1; j < 6; j++) { // write each array element to its corresponding list item
+            listItem = "item" + j;
+            document.getElementById(listItem).innerHTML = places[j];
+        }
+    }
+         
+}
+
+// add backward compatible event listener to Submit button
+var submitButton = document.getElementById("button");
+if (submitButton.addEventListener) {
+    submitButton.addEventListener("click", processInput, false); 
+} else if (submitButton.attachEvent)  {
+    submitButton.attachEvent("onclick", processInput);
+}
